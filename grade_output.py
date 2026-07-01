feaf1bbc0f77dd458a8379bf647ba3c56e0b787a
david <d8087869@gmail.com>
2026-07-01 21:59:08 +0300

    enter to the file the func action and prints

---
+++ grade_output.py
@@ -0,0 +1,11 @@
+def print_student_result(name, grade, status):
+    print(f"{name} , {grade}, {status}")
+
+
+def print_skipped_student(error):
+    print("Skipped student:", error)
+
+
+def print_summary(average, passed):
+    print("Average:", average)
+    print("Passed students:", passed)
